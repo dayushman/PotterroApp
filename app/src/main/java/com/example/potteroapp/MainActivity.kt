a3d586e9d20f9bbeae854143b587eb408e6686e6
ankit <ankit010cse080@gmail.com>
2021-07-28 11:08:57 +0530

    changed variable

--- app/src/main/java/com/example/potteroapp/MainActivity.kt
+++ app/src/main/java/com/example/potteroapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    // dummy code 
+    // dummy code
 
     // Your code
     characterAdapter = CharacterAdapter()
